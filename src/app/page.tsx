0b86df2ab0c041edf122dcec17633978809c860e
Rishabh <kumarrishabh.kumar96@gmail.com>
2025-06-08 17:26:18 +0530

    333

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div className="text-md">
       <main>
-        Hello there! Version 2.0
+        Hello there! Version 3.0
         </main>
     </div>
   );
